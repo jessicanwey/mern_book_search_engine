4fddf2aef1039804373322c455fb7feaeea48644
jessicanwey <jessica.n.wey@gmail.com>
2024-03-11 18:43:21 -0400

    Things are mostly built but there are bugs.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,60 @@
+const User = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if(!profile){
+            throw AuthenticationError;
+        }
+        
+        const correctPassword = await user.isCorrectPassword(password);
+
+        if(!correctPassword){
+            throw AuthenticationError;
+        }
+
+        const token = signToken(user);
+        return { token, user };
+    },
+    addUser: async (parent,args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+
+        return{ token, user };
+    },
+    saveBook: async (parent, {input}, context) => {
+        if(context.user){
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: input }},
+                { new: true, runValidators: true }
+            );
+        } 
+        throw AuthenticationError;
+    },
+    removeBook: async (parent, { bookId }, context) => {
+        if(context.user){
+            return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: {bookId: bookId }}},
+                { new: true }
+            );
+        }
+        throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+//const { gql } = require('@apollo/server');
+
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type BookInput {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookToSave: BookInput): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
