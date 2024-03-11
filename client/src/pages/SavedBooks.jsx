69c951c1dfda39962e5a45ed3917d97cc99c6cee
jessicanwey <jessica.n.wey@gmail.com>
2024-03-11 16:01:13 -0400

    Queries and mutations created plus some formatting

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,23 +1,17 @@
 //import { useState, useEffect } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
 //import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
   //const [userData, setUserData] = useState({});
 
-  const {loading, data} = useQuery(GET_ME);
+  const { loading, data } = useQuery(GET_ME);
 
   //const savedBooks = data?.me.savedBooks || [];
 
@@ -62,30 +56,27 @@ const SavedBooks = () => {
     }
 
     try {
-      await removeBook ({
-        variables: { bookId }
+      await removeBook({
+        variables: { bookId },
       });
 
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
-
     } catch (err) {
       console.error(err);
     }
 
+    //     if (!response.ok) {
+    //       throw new Error('something went wrong!');
+    //     }
 
-  //     if (!response.ok) {
-  //       throw new Error('something went wrong!');
-  //     }
-
-  //     const updatedUser = await response.json();
-  //     setUserData(updatedUser);
-  //     
-   //   removeBookId(bookId);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-
+    //     const updatedUser = await response.json();
+    //     setUserData(updatedUser);
+    //
+    //   removeBookId(bookId);
+    //   } catch (err) {
+    //     console.error(err);
+    //   }
   };
 
   // if data isn't here yet, say so
@@ -101,23 +92,34 @@ const SavedBooks = () => {
         </Container>
       </div>
       <Container>
-        <h2 className='pt-5'>
+        <h2 className="pt-5">
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
         </h2>
         <Row>
           {userData.savedBooks.map((book) => {
             return (
-              // eslint-disable-next-line react/jsx-key//, react/jsx-key
+              // eslint-disable-next-line react/jsx-key
               <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card key={book.bookId} border="dark">
+                  {book.image ? (
+                    <Card.Img
+                      src={book.image}
+                      alt={`The cover for ${book.title}`}
+                      variant="top"
+                    />
+                  ) : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
+                    <p className="small">Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    <Button
+                      className="btn-block btn-danger"
+                      onClick={() => handleDeleteBook(book.bookId)}
+                    >
                       Delete this Book!
                     </Button>
                   </Card.Body>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,61 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const LOGIN_USER = gql``;
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    logingUser(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
-export const ADD_USER = gql``;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
-export const SAVE_BOOK = gql``;
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: bookToSave) {
+    saveBook(input: $input) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
 
-export const DELETE_BOOK = gql``;
+export const DELETE_BOOK = gql`
+  mutation deleteBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
